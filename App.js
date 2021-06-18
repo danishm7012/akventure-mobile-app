f20ce82ec14c402fdd9afc7c58f107363795581b
Danish Mehmood <danishm7012@gmail.com>
2021-06-18 12:33:35 +0500

    fix status bar

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
-import * as Font from 'expo-font';
+import * as Font from 'expo-font'
 
-import React,{useState} from 'react';
+import React, { useState } from 'react'
 
-import AppLoading from 'expo-app-loading';
+import AppLoading from 'expo-app-loading'
 import MainDrawerNavigator from './navigation/MainNavigation'
-import {enableScreens} from 'react-native-screens'
+import { enableScreens } from 'react-native-screens'
+import { StatusBar } from 'expo-status-bar'
 
-enableScreens();
-const fetchFonts = () =>{
- return Font.loadAsync({
+enableScreens()
+const fetchFonts = () => {
+  return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-
-     });
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
 }
 
 export default function App() {
-  let x =1;
-  const [fontloaded, setfontloaded] = useState(false);
+  let x = 1
+  const [fontloaded, setfontloaded] = useState(false)
 
-  if(!fontloaded){
-    return(
+  if (!fontloaded) {
+    return (
       <AppLoading
-      startAsync={fetchFonts}
-      onFinish={() => setfontloaded(true)}
-      onError={console.err}
-  />
+        startAsync={fetchFonts}
+        onFinish={() => setfontloaded(true)}
+        onError={console.err}
+      />
     )
   }
   return (
-    <MainDrawerNavigator/>
-  );
+    <>
+      <StatusBar backgroundColor='#61dafb' style='light' hidden={false} />
+      <MainDrawerNavigator />
+    </>
+  )
 }
-
